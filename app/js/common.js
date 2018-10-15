a3328258ae20d0b19892643db41e7a7c1a85e085
petromath77 <petromath77@gmail.com>
2018-10-15 23:55:04 +0300

    create

---
+++ app/js/common.js
@@ -0,0 +1,30 @@
+$(function() {
+	// function start_count() {	
+	// 	$('.count').each(function() {
+	// 		$(this).prop('Counter', $(this).data('from')).animate({
+	// 		  Counter: $(this).text()
+	// 		}, {
+	// 		  duration: 5000,
+	// 		  easing: 'swing',
+	// 		  step: function(now) {
+	// 			$(this).text(Math.ceil(now))
+	// 		  }
+	// 		})
+	// 	  });
+	// }
+	// $(function() {
+	// 	var oTop = $('.count').offset().top - window.innerHeight;
+	// 	$(window).scroll(function(){
+	// 		var pTop = $(document).scrollTop();
+	// 		if( pTop > oTop ){
+	// 			start_count();
+	// 		}
+	// 	});
+	// });
+
+
+
+
+	// Custom JS
+
+});
